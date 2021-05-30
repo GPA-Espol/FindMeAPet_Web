10ce9009c762083e284b298ad0a9d2f7dcfe14a5
JFNebel <jfnebeldunn@gmail.com>
2021-05-30 17:06:12 -0500

    Ruteo de opciones nav-bar usando @react-router-dom

--- gpa-web-front/src/components/NavBar/NavBar.js
+++ gpa-web-front/src/components/NavBar/NavBar.js
@@ -1,31 +1,83 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './NavBar.css';
-import logo from '../../assets/nav-bar-assets/LogoGPA.png';
+// import logo from '../../assets/nav-bar-assets/LogoGPA.png';
 // import 'assets/'
 // import logo from 'LogoGPA.png'
 
+function NavBar () {
+  return (
+    <React.Fragment>
+      <nav className="navbar">
+        <div className="nav-container">
+          <NavLink exact to="/" className="nav-logo">
+            GPA LOGO
+          </NavLink>
 
-class NavBar extends Component{
+          <ul className="nav-menu">
+            <li className="nav-link">
+              <NavLink exact to="/" className="nav-links">
+                Inicio
+              </NavLink>
+            </li>
+            <li className="nav-link">
+              <NavLink exact to="/quienessomos" className="nav-links">
+                Quienes somos
+              </NavLink>
+            </li>
+            <li className="nav-link">
+              <NavLink exact to="/adoptar" className="nav-links">
+                Adoptar
+              </NavLink>
+            </li>
+            <li className="nav-link">
+              <NavLink exact to="/unete" className="nav-links">
+                Únete
+              </NavLink>
+            </li>
+          </ul>
+          <button className="donar-button" type="button">Click Me!</button>
 
-  render(){
-    return(
-      <React.Fragment>
-        <div className="topnav">
-          <img className="logo" src={logo} alt="" />
-
-          <div className="nav-options">
-            <a className="active" href="#home">Inicio</a>
-            <a href="">Quienes somos</a>
-            <a href="">Adoptar</a>
-            <a href="">Únete</a>
-          </div>
-
-          <button type="button">Click Me!</button>
 
         </div>
-      </React.Fragment>
-    )
-  }
+      </nav>
+    </React.Fragment>
+  )
 }
+
+
+// LO QUE BORRASTE
+
+          {/* <img className="logo" src={logo} alt="" /> */}
+          {/* <div className="nav-options"> */}
+          {/*   <a className="active" href="#home">Inicio</a> */}
+          {/*   <a href="">Quienes somos</a> */}
+          {/*   <a href="">Adoptar</a> */}
+          {/*   <a href="">Únete</a> */}
+          {/* </div> */}
+          {/* <button type="button">Click Me!</button> */}
+
+// class NavBar extends Component{
+
+//   render(){
+//     return(
+//       <React.Fragment>
+//         <div className="topnav">
+//           <img className="logo" src={logo} alt="" />
+
+//           <div className="nav-options">
+//             <a className="active" href="#home">Inicio</a>
+//             <a href="">Quienes somos</a>
+//             <a href="">Adoptar</a>
+//             <a href="">Únete</a>
+//           </div>
+
+//           <button type="button">Click Me!</button>
+
+//         </div>
+//       </React.Fragment>
+//     )
+//   }
+// }
 
 export default NavBar
