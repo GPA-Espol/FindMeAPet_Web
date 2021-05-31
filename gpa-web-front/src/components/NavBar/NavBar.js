51b9085f827c829d7eec6d656ef97af13e96a1cc
JFNebel <jfnebeldunn@gmail.com>
2021-05-30 23:24:49 -0500

    Diseño tentativo de componente Navbar terminado

--- gpa-web-front/src/components/NavBar/NavBar.js
+++ gpa-web-front/src/components/NavBar/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
 import { MenuItems } from '../../fakeBackEnd/navOptions';
-
+import Button from '@material-ui/core/Button';
 
 function NavBar () {
 
@@ -11,14 +11,14 @@ function NavBar () {
       <nav className="navbar">
         <div className="nav-container">
           <NavLink exact to="/" className="nav-logo">
-            GPA LOGO
+            <img src="NavBar/LogoGPA_Navbar.png" alt="GPA Logo"/>
           </NavLink>
 
           {/* TODO: Has dinámico el app.js relacionado */}
           <ul className="nav-menu">
-            {MenuItems.map((item) => {
+            {MenuItems.map((item,index) => {
               return(
-                <li className="nav-link">
+                <li className="nav-link" key={index}>
                   <NavLink exact to={item.url} className="nav-links">
                     {item.nombre}
                   </NavLink>
@@ -26,8 +26,11 @@ function NavBar () {
               );
             })}
           </ul>
-
-          <button className="donar-button" type="button">Click Me!</button>
+          <div className="donar-button-container">
+            <Button variant="contained" color="primary" 
+              style={{color: "#F5f5f5", backgroundColor: "#ea7a1e", fontWeight: "bold"}}>Donar</Button>
+          </div>
+          {/* <button className="donar-button" type="button">Click Me!</button> */}
 
         </div>
       </nav>
