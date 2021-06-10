20b6babb380a7ef3d3b9c6cd5a6d3230c7f6c3e9
JFNebel <jfnebeldunn@gmail.com>
2021-06-10 00:50:58 -0500

    Base para la grilla del catálogo usando react-bootstrap

--- gpa-web-front/src/components/Pages/Adoptar/Adoptar.js
+++ gpa-web-front/src/components/Pages/Adoptar/Adoptar.js
@@ -7,10 +7,136 @@ class PagAdoptar extends Component{
   render(){
     return(
       <div className="pag-adoptar">
-        <h1>Página de adopción con cátalogo</h1>
+        <div className="adoptar-container">
+          <div className="catalogo">
+            <div className="catalogo-container">
+
+
+              {/* TODO: AHORA MIRA SI PUEDES HACERLO CON EL QUE YA TIENE HOVER */}
+
+              {/* The Bootstrap */}
+              <div class="container">
+
+                  <div class="card col-md-3">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div> 
+
+                  <div class="card col-md-3">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div> 
+
+
+                  <div class="card col-md-3">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div> 
+
+                  <div class="card col-md-3">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div> 
+
+              </div>
+
+
+              <div className="container">
+
+                {/* Esto debe tener un salto de linea */}
+                  <div class="card col-md-3">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div> 
+                
+
+                  <div class="card col-md-3">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div> 
+
+                  <div class="card col-md-3">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div> 
+
+                  <div class="card col-md-3">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div> 
+
+
+              </div>
+
+
+
+
+
+
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
+
+
+
+
+              {/* <div className="cat-catalog"> */}
+              {/*   <div className="row"> */}
+              {/*     <div className="row-cols-1 row-cols-md-4"> */}
+              {/*        <div className="hover hover-2 text-white rounded"><img src="" alt=""/> */}
+              {/*          <div className="hover-overlay"></div> */}
+              {/*          <div className="hover-2-content px-5 py-4"> */}
+              {/*            <h3 className="hover-2-title text-uppercase font-weight-bold mb-0"> */} 
+              {/*              <span className="font-weight-light">Image</span> */}
+              {/*              Caption */}
+              {/*            </h3> */}
+              {/*            <p className="hover-2-description text-uppercase mb-0"> */}
+              {/*              Lorem ipsum dolor sit amet, consectetur<br/>adipisicing elit. */}
+              {/*            </p> */}
+              {/*          </div> */}
+              {/*        </div> */}  
+              {/*     </div> */}
+              {/*   </div> */}
+              {/* </div> */}
+
+
+
+
+
+
+
+
+// Chequea estos recursos:
+// 1. https://getbootstrap.com/docs/5.0/components/card/ 
+// 2. https://bootstrapious.com/p/bootstrap-image-hover#:~:text=Bootstrap%20Image%20Hover%20combines%20accessibility,quick%20introduction%20to%20your%20photo 
+// 3. https://www.ordinarycoders.com/blog/article/codepen-bootstrap-card-hovers
 
 export default PagAdoptar
