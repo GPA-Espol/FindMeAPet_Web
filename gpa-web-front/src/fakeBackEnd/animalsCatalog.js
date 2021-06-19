db299caa6d25703b4900ff01e474a45e49b14193
JFNebel <jfnebeldunn@gmail.com>
2021-06-18 21:06:02 -0500

    Nuevos animales Erick

--- gpa-web-front/src/fakeBackEnd/animalsCatalog.js
+++ gpa-web-front/src/fakeBackEnd/animalsCatalog.js
@@ -1,74 +1,74 @@
 export const AnimalItems = [
   {
     "nombre": "Gatito 1",
-    "descripcion": "Descripción 1: Aquí podemos poner una corta descripción, aparte podemos poner campos como edad, sexo (tal vez no para que no los descarten de una), raza, etc.", 
-    "imagen": "Imagen",
+    "descripcion": "Aquí podemos poner una corta descripción, aparte podemos poner campos como sexo, raza, etc.", 
+    "imagen": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
     "url": "/"
   },
   {
     "nombre": "Gatito 2",
-    "descripcion": "Descripción 2: Aquí podemos poner una corta descripción, aparte podemos poner campos como edad, sexo (tal vez no para que no los descarten de una), raza, etc.", 
-    "imagen": "Imagen",
+    "descripcion": "Aquí podemos poner una corta descripción, aparte podemos poner campos como sexo, raza, etc.", 
+    "imagen": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
     "url": "/"
   },
   {
     "nombre": "Gatito 3",
-    "descripcion": "Descripción 3: Aquí podemos poner una corta descripción, aparte podemos poner campos como edad, sexo (tal vez no para que no los descarten de una), raza, etc.", 
-    "imagen": "Imagen",
+    "descripcion": "Aquí podemos poner una corta descripción, aparte podemos poner campos como sexo, raza, etc.", 
+    "imagen": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
     "url": "/"
   },
   {
     "nombre": "Gatito 4",
-    "descripcion": "Descripción 4: Aquí podemos poner una corta descripción, aparte podemos poner campos como edad, sexo (tal vez no para que no los descarten de una), raza, etc.", 
-    "imagen": "Imagen",
+    "descripcion": "Aquí podemos poner una corta descripción, aparte podemos poner campos como sexo, raza, etc.", 
+    "imagen": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
     "url": "/"
   },
   {
     "nombre": "Gatito 5",
-    "descripcion": "Descripción 5: Aquí podemos poner una corta descripción, aparte podemos poner campos como edad, sexo (tal vez no para que no los descarten de una), raza, etc.", 
-    "imagen": "Imagen",
+    "descripcion": "Aquí podemos poner una corta descripción, aparte podemos poner campos como sexo, raza, etc.", 
+    "imagen": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
     "url": "/"
   },
   {
     "nombre": "Gatito 6",
-    "descripcion": "Descripción 6: Aquí podemos poner una corta descripción, aparte podemos poner campos como edad, sexo (tal vez no para que no los descarten de una), raza, etc.", 
-    "imagen": "Imagen",
+    "descripcion": "Aquí podemos poner una corta descripción, aparte podemos poner campos como sexo, raza, etc.", 
+    "imagen": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
     "url": "/"
   },
   {
     "nombre": "Gatito 7",
-    "descripcion": "Descripción 7: Aquí podemos poner una corta descripción, aparte podemos poner campos como edad, sexo (tal vez no para que no los descarten de una), raza, etc.", 
-    "imagen": "Imagen",
+    "descripcion": "Aquí podemos poner una corta descripción, aparte podemos poner campos como sexo, raza, etc.", 
+    "imagen": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
     "url": "/"
   },
   {
     "nombre": "Gatito 8",
-    "descripcion": "Descripción 8: Aquí podemos poner una corta descripción, aparte podemos poner campos como edad, sexo (tal vez no para que no los descarten de una), raza, etc.", 
-    "imagen": "Imagen",
+    "descripcion": "Aquí podemos poner una corta descripción, aparte podemos poner campos como sexo, raza, etc.", 
+    "imagen": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
     "url": "/"
   },
   {
     "nombre": "Gatito 9",
-    "descripcion": "Descripción 9: Aquí podemos poner una corta descripción, aparte podemos poner campos como edad, sexo (tal vez no para que no los descarten de una), raza, etc.", 
-    "imagen": "Imagen",
+    "descripcion": "Aquí podemos poner una corta descripción, aparte podemos poner campos como sexo, raza, etc.", 
+    "imagen": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
     "url": "/"
   },
   {
     "nombre": "Gatito 10",
-    "descripcion": "Descripción 10: Aquí podemos poner una corta descripción, aparte podemos poner campos como edad, sexo (tal vez no para que no los descarten de una), raza, etc.", 
-    "imagen": "Imagen",
+    "descripcion": "Aquí podemos poner una corta descripción, aparte podemos poner campos como sexo, raza, etc.", 
+    "imagen": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
     "url": "/"
   },
   {
     "nombre": "Gatito 11",
-    "descripcion": "Descripción 11: Aquí podemos poner una corta descripción, aparte podemos poner campos como edad, sexo (tal vez no para que no los descarten de una), raza, etc.", 
-    "imagen": "Imagen",
+    "descripcion": "Aquí podemos poner una corta descripción, aparte podemos poner campos como sexo, raza, etc.", 
+    "imagen": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
     "url": "/"
   },
   {
     "nombre": "Gatito 12",
-    "descripcion": "Descripción 12: Aquí podemos poner una corta descripción, aparte podemos poner campos como edad, sexo (tal vez no para que no los descarten de una), raza, etc.", 
-    "imagen": "Imagen",
+    "descripcion": "Aquí podemos poner una corta descripción, aparte podemos poner campos como sexo, raza, etc.", 
+    "imagen": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
     "url": "/"
   },
 ]
